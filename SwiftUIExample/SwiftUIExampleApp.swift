e8755be7e0ee080bd2e9dd41db07a1c965dfb8f1
yagoal <br.yago@gmail.com>
2024-05-04 19:18:32 -0300

    Initial Commit

---
+++ SwiftUIExample/SwiftUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIExampleApp.swift
+//  SwiftUIExample
+//
+//  Created by Yago Pereira on 4/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
